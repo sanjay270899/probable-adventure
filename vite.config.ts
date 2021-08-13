bca2dd30886420d87da946514518e16e040b6ce1
Pathetic Geek <geekpathetic@gmail.com>
2021-08-13 11:20:04 +0530

    ✨ Fix vite path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@assets': './src/assets',
+      '@components': './src/components',
+      '@config': './src/config',
+      '@hooks': './src/hooks',
+      '@interfaces': './src/interfaces',
+      '@pages': './src/pages',
+      '@services': './src/services',
+      '@state': './src/state',
+      '@utils': './src/utils'
+    }
+  }
 })
