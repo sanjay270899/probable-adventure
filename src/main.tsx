1ada0037d4c015c93b03ef02d0b0f28971a0ec8d
Pathetic Geek <geekpathetic@gmail.com>
2021-08-19 15:17:12 +0530

    ✨ Update sentry add markdown

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import mixpanel from 'mixpanel-browser'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Toaster } from 'react-hot-toast'
@@ -7,12 +8,26 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { queryClient } from '@config/query.config'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import { store } from '@state/index'
 
 import 'twind/shim'
 import '@utils/twindConfig'
 
 import App from './App'
+
+if (import.meta.env.VITE_SENTRY_URL) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_URL,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.8
+  })
+}
+
+if (import.meta.env.VITE_MIXPANEL_TOKEN) {
+  mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN)
+}
 
 ReactDOM.render(
   <React.StrictMode>
